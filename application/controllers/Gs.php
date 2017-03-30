fd877726653723211c1451ceff0a08f620e3ae21
Guilherme Cardoso <guicardoso@gmail.com>
2017-03-30 14:47:07 -0300

    Added groups suport

--- application/controllers/Gs.php
+++ application/controllers/Gs.php
@@ -5,6 +5,13 @@ class Gs extends CI_Controller {
 
 	var $gsconfig = array();
 
+	function __construct() {
+
+		parent::__construct();
+
+        $this->load->model('ext_configs');
+    }
+
 	public function index()
 	{
 		echo "string";
@@ -15,7 +22,9 @@ class Gs extends CI_Controller {
 		if ( preg_match('/cfg([0-f]+)\.xml/', $filename, $match) )
 		{
 			$mac = strtolower($match[1]);
-			$this->xmlconfig($mac);
+			$this->output
+				->set_content_type('application/xml')
+				->set_output($this->ext_configs->xmlconfig($mac));
 			return;
 		}
 
@@ -30,85 +39,6 @@ class Gs extends CI_Controller {
 		
 		show_404();
 	
-	}
-
-	private function xmlconfig($mac)
-	{
-		$this->gsgeral();
-		$this->gsuser($mac);
-
-		$xml = new SimpleXMLElement("<?xml version='1.0' encoding='utf-8'?><gs_provision/>");
-		$xml->addAttribute('version', '1');
-		$xml->addChild("mac", $mac);
-		$xml->addChild("config")
-			->addAttribute('version', '1');
-		
-
-		foreach ($this->gsconfig as $k => $v)
-		{
-		  $xml->config->addChild($k, $v);
-		}
-
-		$this->output
-			->set_content_type('application/xml')
-			->set_output($xml->saveXML());
-	}
-
-	private function gsgeral()
-	{
-		if ( $file = fopen("data/global.txt","r") )
-		{
-			while(! feof($file))
-			{
-				$line = fgets($file);
-				$uline = explode('#', $line);
-
-				$pv = explode('=', $uline[0]);
-
-				# Filtra linhas com comentários
-				if ( count($pv) < 2 )
-					continue;
-
-				$this->gsconfig[trim($pv[0])] = trim($pv[1]);
-			}
-
-			fclose($file);
-		}
-
-		else
-			throw new Exception("Erro no global.txt", 1);
-	}
-
-	private function gsuser($mac)
-	{
-		if ( $file = fopen("data/extensions.txt","r") )
-		{
-			while(! feof($file))
-			{
-				$line = fgets($file);
-				$uline = explode('#', $line);
-				$fields = explode(';', $uline[0]);
-
-				# Filtra linhas com comentários
-				if ( count($fields) < 2 )
-					continue;
-
-				if ( strtolower(trim($fields[0])) == $mac )
-				{
-					array_shift($fields);
-					foreach ($fields as $value)
-					{
-						$pv = explode('=', $value);
-						$this->gsconfig[trim($pv[0])] = trim($pv[1]);
-					}
-					break;
-				}
-			}
-
-			fclose($file);
-		}
-		else
-			throw new Exception("Erro no extensions.txt", 1);
 	}
 
 }
---
+++ application/models/Ext_configs.php
@@ -0,0 +1,108 @@
+<?php
+#TODO tratamento do undefined (mac que não está na lista)
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Ext_configs extends CI_Model {
+
+	var $gsconfig = array();
+	var $groups = 'undefined';
+
+	
+	public function xmlconfig($mac)
+	{
+		$this->cfg_load_ext($mac);
+
+		$xml = new SimpleXMLElement("<?xml version='1.0' encoding='utf-8'?><gs_provision/>");
+		$xml->addAttribute('version', '1');
+		$xml->addChild("mac", $mac);
+		$xml->addChild("config")
+			->addAttribute('version', '1');
+		
+
+		foreach ($this->gsconfig as $k => $v)
+		{
+		  $xml->config->addChild($k, $v);
+		}
+
+		return $xml->saveXML();
+	}
+
+	private function cfg_load_group($group)
+	{
+		if ( $file = fopen("data/".$group.".txt","r") )
+		{
+			while(! feof($file))
+			{
+				$line = fgets($file);
+				$uline = explode('#', $line);
+
+				$pv = explode('=', $uline[0]);
+
+				# Filtra linhas com comentários
+				if ( count($pv) < 2 )
+					continue;
+
+				$this->gsconfig[trim($pv[0])] = trim($pv[1]);
+			}
+
+			fclose($file);
+		}
+
+		else
+			throw new Exception("Erro no ".$group.".txt", 1);
+	}
+
+	private function cfg_load_groups($groups)
+	{
+		$vgroups = explode('/', $groups);
+
+		foreach ($vgroups as $group) {
+			$this->cfg_load_group($group);
+		}
+	}
+
+	private function cfg_load_ext($mac)
+	{
+		if ( $file = fopen("data/extensions.txt","r") )
+		{
+			while(! feof($file))
+			{
+				$line = fgets($file);
+				$uline = explode('#', $line);
+
+				# Procura os groupos
+				if ( preg_match('/\[\s*((?>\w+\/?)+)\s*\]/', $uline[0], $match) )
+				{
+					$groups = trim( strtolower($match[1]), '/');
+					continue;
+				}
+
+				$fields = explode(';', $uline[0]);
+
+				# Filtra linhas com comentários
+				if ( count($fields) < 2 )
+					continue;
+
+				if ( strtolower(trim($fields[0])) == $mac )
+				{
+					array_shift($fields);
+					
+					$this->cfg_load_groups($groups);
+
+					$this->groups = $groups;
+
+					foreach ($fields as $value)
+					{
+						$pv = explode('=', $value);
+						$this->gsconfig[trim($pv[0])] = trim($pv[1]);
+					}
+					break;
+				}
+			}
+
+			fclose($file);
+		}
+		else
+			throw new Exception("Erro no extensions.txt", 1);
+	}
+
+}
